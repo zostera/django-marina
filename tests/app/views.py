96a5ba7c63c572e19a2c4b548b7017a3c08a2740
Dylan Verheul <dylan@zostera.nl>
2020-06-15 08:37:39 +0200

    Simplify imports and add tests

--- tests/app/views.py
+++ tests/app/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponse
+from django.utils.html import format_html
 from django.views import View
 
 
 class HomeView(View):
-    def get(self, request):
+    def get(self, *args, **kwargs):
         return HttpResponse("<html><head><title>Hi</title></head><body><h1 class='title'>Hi</h1></body></html>")
 
 
@@ -15,3 +16,14 @@ class LoginRequiredView(LoginRequiredMixin, HomeView):
 class SuperuserRequiredView(PermissionRequiredMixin, HomeView):
     def has_permission(self):
         return self.request.user.is_superuser
+
+
+class EchoView(View):
+    def _response(self, data):
+        return HttpResponse(format_html("<html><head><title>Echo</title></head><body>{data}</body></html>", data=data))
+
+    def get(self, *args, **kwargs):
+        return self._response(data=self.request.GET)
+
+    def post(self, *args, **kwargs):
+        return self._response(data=self.request.POST)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import PermissionDenied
 from django.test import TestCase
 
-from django_marina.db.migrations import DisableMigrations
+from django_marina.db import DisableMigrations
 
 from .models import ProtectedModel
 
