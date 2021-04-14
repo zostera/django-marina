0ba8aa83e5e4a076cf6991c96fcc7f4c7919ecf2
Dylan Verheul <dylan@dyve.net>
2021-04-14 11:19:57 +0200

    Add information about changed fields to ProtectedModelMixin

--- src/django_marina/db/models.py
+++ src/django_marina/db/models.py
@@ -17,6 +17,48 @@ class ProtectedModelMixin:
     # Default message shown to user when object has delete protection.
     delete_protection_message = gettext_lazy("This object has delete protection.")
 
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        """
+        Store fields and values from database in instance for reference.
+
+        Source: https://docs.djangoproject.com/en/3.1/ref/models/instances/#customizing-model-loading
+        """
+        instance = super().from_db(db, field_names, values)
+        instance._loaded_values = dict(zip(field_names, values))
+        return instance
+
+    def get_changed_fields(self, fields=None):
+        """
+        Return dictionary of fields with loaded values that have changed.
+
+        :param fields: List of fields, defaults to all fields
+        :return: Dictionary with fields and old values that have changed
+        """
+        if self._state.adding:
+            return {}
+        try:
+            loaded_values = self._loaded_values
+        except AttributeError:
+            return {}
+        if not fields:
+            fields = list(loaded_values.keys())
+        changed_fields = {}
+        for field in fields:
+            loaded_value = loaded_values[field]
+            if getattr(self, field) != loaded_value:
+                changed_fields[field] = loaded_value
+        return changed_fields
+
+    def has_changed_fields(self, fields=None):
+        """
+        Return whether any of the given fields (defaults to all fields) have changed.
+
+        :param fields: List of fields, defaults to all fields
+        :return: Bool whether or not any of the fields have changed
+        """
+        return bool(self.get_changed_fields(fields))
+
     @property
     def has_update_protection(self):
         """Return whether this object has update protection."""
@@ -28,7 +70,7 @@ class ProtectedModelMixin:
 
     def save(self, *args, **kwargs):
         """Save the instance, unless it has a primary key and update protection."""
-        if self.pk and self.has_update_protection:
+        if self.has_update_protection and not self._state.adding:
             raise PermissionDenied(self.get_update_protection_message())
         return super().save(*args, **kwargs)
 
--- tests/test_db.py
+++ tests/test_db.py
@@ -7,8 +7,43 @@ from .models import ProtectedModel
 
 
 class ProtectedModelMixinTestCase(TestCase):
+    def test_protected_model_get_changed_fields(self):
+        protected = ProtectedModel(name="protected", is_update_protected=True)
+        self.assertEqual(protected.get_changed_fields(), {})
+        protected.name = "changed"
+        self.assertEqual(protected.get_changed_fields(), {})
+        protected.save()
+        protected = ProtectedModel.objects.get(pk=protected.pk)
+        self.assertEqual(protected.get_changed_fields(), {})
+        protected.name = "changed"
+        self.assertEqual(protected.get_changed_fields(), {})
+        protected.name = "another change"
+        self.assertEqual(protected.get_changed_fields(), {"name": "changed"})
+        self.assertEqual(protected.get_changed_fields(["is_update_protected"]), {})
+
+    def test_protected_model_has_changed_fields(self):
+        protected = ProtectedModel(name="protected", is_update_protected=True)
+        self.assertFalse(protected.has_changed_fields())
+        protected.name = "changed"
+        self.assertFalse(protected.has_changed_fields())
+        protected.save()
+        protected = ProtectedModel.objects.get(pk=protected.pk)
+        self.assertFalse(protected.has_changed_fields())
+        protected.name = "changed"
+        self.assertFalse(protected.has_changed_fields())
+        protected.name = "another change"
+        self.assertTrue(protected.has_changed_fields())
+        self.assertFalse(protected.has_changed_fields(["is_update_protected"]))
+
     def test_update_protected(self):
         protected = ProtectedModel(name="protected", is_update_protected=True)
+        protected.save()
+        with self.assertRaises(PermissionDenied):
+            protected.save()
+
+    def test_update_protected_before_create(self):
+        protected = ProtectedModel(name="protected", is_update_protected=True)
+        protected.name = "change"
         protected.save()
         with self.assertRaises(PermissionDenied):
             protected.save()
