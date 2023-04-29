f51868b50f19abf996755cbc3550f81c8e827a16
Dylan Verheul <dylan@dyve.net>
2023-04-29 11:09:15 +0200

    Back to importlib_metadata

--- src/django_marina/__init__.py
+++ src/django_marina/__init__.py
@@ -3,6 +3,10 @@ __all__ = [
 ]
 
 
-import importlib.metadata
+# TODO: Remove after Python 3.7 support gets dropped (py37)
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata as importlib_metadata
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+__version__ = importlib_metadata.version(__package__ or __name__)
