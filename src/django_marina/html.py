462e3fa4c5b06614af93728a513ebbe84db3987e
Dylan Verheul <dylan@dyve.net>
2023-12-26 15:35:48 +0100

    Add ignore_attrs to assertHTMLEqual

---
+++ src/django_marina/html.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+
+def remove_attrs(html, attrs=None):
+    """Return html string without the specified tags."""
+    if html and attrs:
+        soup = BeautifulSoup(html, "html.parser")
+        soup = _remove_attrs(soup, attrs)
+        html = f"{soup}"
+    return html
+
+
+def _remove_attrs(soup, attrs):
+    for attr in attrs:
+        for tag in soup.findAll(attrs={attr: True}):
+            del tag.attrs[attr]
+    return soup
