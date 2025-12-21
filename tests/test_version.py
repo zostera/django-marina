6ba7b724f2cea8b76dd4de76e7c514dea95ba232
Dylan Verheul <dylan@dyve.net>
2025-12-21 09:55:18 +0100

    Check CI

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.test import TestCase
 
 
@@ -10,3 +12,13 @@ class VersionTest(TestCase):
         version = django_marina.__version__
         version_parts = version.split(".")
         self.assertTrue(len(version_parts) in (2, 3))
+
+        try:
+            import django
+
+            django_version = django.get_version()
+        except Exception:
+            django_version = "not available"
+
+        if "test" in sys.argv:
+            print(f"* Python {sys.version.split()[0]}\n* Django {django_version}")
