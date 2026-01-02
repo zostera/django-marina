d57713a2904437930f41982b6aba69c654c6b704
Dylan Verheul <dylan@dyve.net>
2026-01-02 14:02:38 +0100

    Add tests

--- tests/test_extended_client.py
+++ tests/test_extended_client.py
@@ -1,7 +1,10 @@
+from django.template import engines
+from django.template.response import SimpleTemplateResponse
 from django.test import TestCase
 from django.urls import reverse
 
 from django_marina.test import ExtendedClient
+from django_marina.test.clients import _get_soup
 
 
 class ExtendedClientTestCase(TestCase):
@@ -21,3 +24,11 @@ class ExtendedClientTestCase(TestCase):
         response = client.post(self.url_echo, data={"foo": "bar"})
         self.assertContains(response, "foo")
         self.assertContains(response, "bar")
+
+    def test_get_soup_renders_template_response(self):
+        template = engines["django"].from_string("<html><body><p>Hello</p></body></html>")
+        response = SimpleTemplateResponse(template=template, context={})
+        self.assertFalse(response.is_rendered)
+        soup = _get_soup(response)
+        self.assertTrue(response.is_rendered)
+        self.assertEqual(soup.p.text, "Hello")
